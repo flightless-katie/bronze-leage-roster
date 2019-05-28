bcb801c81f664483192de491f8b7ce1612a2b74f
Katie Snead <katiekat689@gmail.com>
2019-05-27 22:12:48 -0400

    Add more stuff to roster

--- src/roster.js
+++ src/roster.js
@@ -21,72 +21,72 @@ export default [
   },
   {
     name: "SpicySquid",
-    battletags: [],
-    heroes: ["Mercy"],
-    roles: [],
+    battletags: ["SpicySquid#11456"],
+    heroes: ["Lucio", "Mercy", "Zenyatta", "Moira", "Ana"],
+    roles: ["Support", "Tank", "DPS"],
     timezone: "EST"
   },
   {
     name: "Filandia",
-    battletags: [],
-    heroes: ["Moira"],
-    roles: [],
+    battletags: ["Filandia#1196"],
+    heroes: ["Mercy", "Ana", "Dva", "Brigitte", "Moira"],
+    roles: ["Support", "Tank", "DPS"],
     timezone: "EST"
   },
   {
     name: "ShaolinSavvy",
-    battletags: [],
-    heroes: ["Mercy"],
-    roles: [],
+    battletags: ["ShaolinSavvy#1708"],
+    heroes: ["Mercy", "Moira", "Brigitte", "Baptiste", "Lucio"],
+    roles: ["Support", "DPS", "Tank"],
     timezone: "EST"
   },
   {
     name: "Ebbie",
-    battletags: [],
-    heroes: ["Doomfist"],
-    roles: [],
+    battletags: ["Ebbie#11117"],
+    heroes: ["Doomfist", "Reinhardt"],
+    roles: ["DPS", "Tank", "Support"],
     timezone: "EST"
   },
   {
     name: "Xachine",
-    battletags: [],
-    heroes: ["Dva"],
-    roles: [],
+    battletags: ["xachine#1289"],
+    heroes: ["Dva", "Reinhardt", "Zenyatta", "Orisa", "Mercy"],
+    roles: ["Tank", "Support", "DPS"],
     timezone: "EST"
   },
   {
     name: "NightRaven",
-    battletags: [],
-    heroes: ["Mercy"],
-    roles: [],
+    battletags: ["NightRaven#138844"],
+    heroes: ["Mercy", "Dva", "Moira", "Brigitte", "Lucio"],
+    roles: ["Support", "Tank", "DPS"],
     timezone: "EST"
   },
   {
     name: "Kua",
-    battletags: [],
-    heroes: ["Reinhardt"],
-    roles: [],
+    battletags: ["Kua#11386"],
+    heroes: ["Reinhardt", "Zarya"],
+    roles: ["Tank", "DPS", "Support"],
     timezone: "EST"
   },
   {
     name: "Gold Talisman",
-    battletags: [],
-    heroes: ["Pharah"],
+    battletags: ["goldtalisman#1160"],
+    heroes: ["Pharah", "Mercy", "Mei", "Moira"],
     roles: [],
     timezone: "EST"
   },
   {
     name: "Chewbacca For You",
-    battletags: [],
-    heroes: ["Pharah"],
-    roles: [],
+    battletags: ["Chewbacca#12731"],
+    heroes: ["Pharah", "Widowmaker", "Mei", "Reaper", "Junkrat"],
+    roles: ["DPS", "Tank", "Support"],
     timezone: "EST"
   },
   {
-    name: "ffanatic13",
-    battletags: [],
-    heroes: ["Reinhardt"],
-    roles: [],
+    name: "Fanatic",
+    battletags: ["ffanatic13#1942"],
+    heroes: ["Reinhardt", "Winston", "Orisa", "Hammond", "Moira"],
+    roles: ["Tank", "Support", "DPS"],
     timezone: "EST"
   },
   {
